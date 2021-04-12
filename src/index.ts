913e520aa49adc7bb5e5d2a0097127d0f2e17d8d
Жулинский Владислав Сергеевич <vladislav.zhulinskiy@telematika.com>
2021-04-12 18:48:43 +0300

    test socket

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,16 @@ import express, { Application } from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
 import multer from 'multer'
-import bodyParser from 'body-parser'
 // @ts-ignore
 import EasyYandexS3  from 'easy-yandex-s3'
 import settings from './settings'
 import router from './router'
 import adapterDBConnector from './adapter-db-connector'
 import { errorFeedBack } from './FeedBack'
+import { createServer  } from 'http'
+// @ts-ignore
+import io, { Socket } from 'socket.io'
+
 
 export const s3 = new EasyYandexS3({
   auth: {
@@ -21,27 +24,43 @@ export const s3 = new EasyYandexS3({
 
 class Server {
   private app: Application
+  private server: any
+  private io: any
   constructor() {
     this.app = express()
+    this.server = createServer(this.app)
+    const options = {
+      // wsEngine: 'eiows',
+      cors: {
+        origin: '*',
+      }
+    }
+    // @ts-ignore
+    this.io = io(this.server, options)
     this.config()
     this.routes()
   }
   private config(): void {
     // our plugins
-    this.app.set('PORT', settings.PORT)
+    // this.app.set('PORT', settings.PORT)
     this.app.use(express.json())
     this.app.use(helmet())
     this.app.use(cors())
     this.app.use(multer().any())
-    this.app.use(bodyParser.json())
-    this.app.use(bodyParser.urlencoded())
-    this.app.use(bodyParser.urlencoded({ extended: true }))
   }
   private routes(): void {
     this.app.use(router)
   }
   private startServer(): void {
-    this.app.listen(this.app.get('PORT'), () => console.log(`Server started on ${settings.DOMAIN}`))
+    //Whenever someone connects this gets executed
+    // this.io.on('connection', function(socket: Socket) {
+    //   console.log('A user connected', socket);
+    //   //Whenever someone disconnects this piece of code executed
+    //   socket.on('disconnect', function () {
+    //     console.log('A user disconnected');
+    //   });
+    // });
+    this.server.listen(settings.PORT, () => console.log(`Server started on ${settings.DOMAIN}`))
   }
   public async init() {
     try {
